630bf0bf3a0ccd93240899d9969f7e9657e2222d
Sander <sandersieglaff@gmail.com>
2022-12-13 19:31:31 -0600

    Dockerized everything

--- frontend/frontend/asgi.py
+++ frontend/frontend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for herptracker project.
+ASGI config for frontend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'herptracker.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'frontend.settings')
 
 application = get_asgi_application()
